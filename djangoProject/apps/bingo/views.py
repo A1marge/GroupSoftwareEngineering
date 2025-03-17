4cddc78723a50b01cdd2b1d8d928635277d38013
Yoav-Shimoni <139874837+Yoav-Shimoni@users.noreply.github.com>
2025-03-17 12:38:51 +0000

    Reverted-Bingo-Board-Changes

--- djangoProject/apps/bingo/views.py
+++ djangoProject/apps/bingo/views.py
@@ -20,7 +20,7 @@ def make_board(rows,cols):
     for i in range(0,4):
         for j in range(0,4):
             challenge = numbers.pop()
-            board[i][j] = {"status": bool(random.getrandbits(1)), "challenge": "Challenge "+str(challenge), "url": "/games/game" +str(challenge)+"/"}
+            board[i][j] = {"status": False, "challenge": challenge, "url": "/games/game" +str(challenge)+"/"}
 
     return board
 
